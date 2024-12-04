82cbf7236fde46f93e69102fe48be862a04c362b
ankur0904 <ankursingh91002@gmail.com>
2024-12-04 13:42:01 +0530

    anlytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import './styles/index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>
 );
